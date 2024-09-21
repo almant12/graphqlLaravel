0e77ce9ee7b2da000bce90c89ab946cf3583d151
almant12 <dokoalmant123@gmail.com>
2024-09-21 16:29:03 +0200

    complete AuthorType for graphql

--- app/GraphQL/Types/AuthorType.php
+++ app/GraphQL/Types/AuthorType.php
@@ -2,13 +2,49 @@
 
 namespace App\GraphQL\Types;
 
-class AuthorType
+use App\Models\User;
+use GraphQL\Type\Definition\Type;
+use Rebing\GraphQL\Support\Facades\GraphQL;
+use Rebing\GraphQL\Support\Type as GraphQLType;
+
+class AuthorType extends GraphQLType
 {
-    /**
-     * Create a new class instance.
-     */
-    public function __construct()
-    {
-        //
+
+
+    protected $attributes = [
+        'name' => 'Book',
+        'description' => 'collection of Author',
+        'model' => User::class
+    ];
+
+
+    public function fields(): array{
+        return [
+            'id'=>[
+                'type'=>Type::nonNull(Type::int()),
+                'description'=>'id of author'
+            ],
+            'name'=>[
+                'type'=>Type::nonNull(Type::string()),
+                'description'=>'name of author'
+            ],
+            'bio'=>[
+                'type'=>Type::nonNull(Type::string()),
+                'description'=>'bio of author'
+            ],
+            'books'=>[
+                'type'=>Type::listOf(GraphQL::type('Book')),
+                'description'=>'a lost of books for author'
+            ],
+            'created_at' => [
+            'type' => Type::nonNull(Type::string()),
+            'description' => 'The timestamp when the category was created',
+            ],
+
+            'updated_at' => [
+            'type' => Type::nonNull(Type::string()),
+            'description' => 'The timestamp when the category was last updated',
+            ],
+        ];
     }
 }
