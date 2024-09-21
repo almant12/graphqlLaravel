b5309b40d758178ca891fb87fafd10ebc2b104fc
almant12 <dokoalmant123@gmail.com>
2024-09-21 13:19:44 +0200

    relationship for book model

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -4,9 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+use function PHPUnit\Framework\returnCallback;
 
 class Book extends Model
 {
     use HasFactory;
 
+    protected $fillable = ['title','category_id','description'];
+
+    public function category():BelongsTo{
+        return $this->belongsTo(Category::class);
+    }
+
+    public function authors():BelongsToMany{
+        return $this->belongsToMany(User::class,'author_book');
+    }
 }
