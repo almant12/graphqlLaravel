5a67814a09a4f70d0e15acb26b5748ec8c995885
almant12 <dokoalmant123@gmail.com>
2024-09-21 16:45:40 +0200

    define query for each model

---
+++ app/GraphQL/Queries/Author/AuthorQuery.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\GraphQL\Queries\Author;
+
+class AuthorQuery
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
---
+++ app/GraphQL/Queries/Book/BooksQuery.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\GraphQL\Queries\Book;
+
+class BooksQuery
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
